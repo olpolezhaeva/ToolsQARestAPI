8d321388e32c8e3c58b02a26d4f597fa2f5f9b5d
azellus-tercia <azellustercia@gmail.com>
2022-08-25 16:12:56 +0500

    Creation of user and checks were added. Minor fix.

--- src/test/java/runner/BaseRunner.java
+++ src/test/java/runner/BaseRunner.java
@@ -2,12 +2,37 @@ package runner;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import model.CreateUserPostJson;
-import org.testng.annotations.BeforeClass;
+import org.testng.annotations.*;
 
 public abstract class BaseRunner {
 
     private String tokenAPI;
+    private String userId;
+
+    @BeforeSuite
+    protected void createUser() {
+        RestAssured.baseURI = EndPoints.BASE_API_URL;
+
+        Response response = RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(new CreateUserPostJson(BaseProperties.getProperties().getProperty("username"),
+                        BaseProperties.getProperties().getProperty("password")))
+                .post(EndPoints.PAGE_ACCOUNT_USER);
+
+        if (response.getStatusCode() != 201) {
+            throw new RuntimeException("Error: " + response.getBody().jsonPath().get("message").toString());
+        } else {
+            userId = response
+                    .getBody()
+                    .jsonPath()
+                    .get("userID")
+                    .toString();
+        }
+    }
 
     @BeforeClass
     protected void setTokenAPI() {
@@ -16,8 +41,6 @@ public abstract class BaseRunner {
 //                .setContentType(ContentType.JSON)
 //                .setAccept(ContentType.JSON)
 //                .build();
-
-        RestAssured.baseURI = EndPoints.BASE_API_URL;
 
         tokenAPI = RestAssured
                 .given()
@@ -31,7 +54,22 @@ public abstract class BaseRunner {
                 .toString();
     }
 
+    @AfterSuite
+    protected void deleteUser() {
+        RestAssured
+                .given()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .auth()
+                .oauth2(tokenAPI)
+                .delete(EndPoints.PAGE_ACCOUNT_USER + "/" + userId);
+    }
+
     protected String getTokenAPI() {
         return tokenAPI;
+    }
+
+    protected String getUserId() {
+        return userId;
     }
 }
